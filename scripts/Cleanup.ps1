708d19a50a8e2715f23466923d042721f398e253
Freddy Kristiansen <freddyk@microsoft.com>
2020-11-06 07:15:23 +0100

    initial commit

---
+++ scripts/Cleanup.ps1
@@ -0,0 +1,38 @@
+﻿Param(
+    [Parameter(Mandatory=$false)]
+    [ValidateSet('Local', 'AzureDevOps', 'GithubActions', 'GitLab')]
+    [string] $environment = 'Local'
+)
+
+. (Join-Path $PSScriptRoot "Read-Settings.ps1") -environment $environment
+
+if ("$AgentName" -ne "Hosted Agent" -and "$AgentName" -notlike "Azure Pipelines*") {
+    . (Join-Path $PSScriptRoot "Install-BcContainerHelper.ps1") -bcContainerHelperVersion $bcContainerHelperVersion
+
+    $cleanupMutexName = "Cleanup"
+    $cleanupMutex = New-Object System.Threading.Mutex($false, $cleanupMutexName)
+    try {
+        try {
+            if (!$cleanupMutex.WaitOne(1000)) {
+                Write-Host "Waiting for other process to finish cleanup"
+                $cleanupMutex.WaitOne() | Out-Null
+                Write-Host "Other process completed"
+            }
+        }
+        catch [System.Threading.AbandonedMutexException] {
+           Write-Host "Other process terminated abnormally"
+        }
+    
+        Remove-BcContainer -containerName $containerName
+        Flush-ContainerHelperCache -KeepDays 2
+        
+        Remove-Module BcContainerHelper
+        $path = Join-Path $ENV:Temp $containerName
+        if (Test-Path $path) {
+            Remove-Item $path -Recurse -Force
+        }
+    }
+    finally {
+        $cleanupMutex.ReleaseMutex()
+    }
+}
---
+++ scripts/DevOps-Pipeline.ps1
@@ -0,0 +1,83 @@
+﻿Param(
+    [Parameter(Mandatory=$false)]
+    [ValidateSet('AzureDevOps','GithubActions','GitLab')]
+    [string] $environment = 'AzureDevOps',
+    [Parameter(Mandatory=$true)]
+    [string] $version,
+    [Parameter(Mandatory=$false)]
+    [int] $appBuild = 0,
+    [Parameter(Mandatory=$false)]
+    [int] $appRevision = 0
+)
+
+if ($environment -eq "AzureDevOps") {
+    $buildArtifactFolder = $ENV:BUILD_ARTIFACTSTAGINGDIRECTORY
+}
+elseif ($environment -eq "GitHubActions") {
+    $buildArtifactFolder = Join-Path $ENV:GITHUB_WORKSPACE "output"
+    New-Item $buildArtifactFolder -ItemType Directory | Out-Null
+}
+
+$baseFolder = (Get-Item (Join-Path $PSScriptRoot "..")).FullName
+. (Join-Path $PSScriptRoot "Read-Settings.ps1") -environment $environment -version $version
+. (Join-Path $PSScriptRoot "Install-BcContainerHelper.ps1") -bcContainerHelperVersion $bcContainerHelperVersion -genericImageName $genericImageName
+
+$params = @{}
+$insiderSasToken = "$ENV:insiderSasToken"
+$licenseFile = "$ENV:licenseFile"
+$codeSigncertPfxFile = "$ENV:CodeSignCertPfxFile"
+if (!$doNotSignApps -and $codeSigncertPfxFile) {
+    if ("$ENV:CodeSignCertPfxPassword" -ne "") {
+        $codeSignCertPfxPassword = try { "$ENV:CodeSignCertPfxPassword" | ConvertTo-SecureString } catch { ConvertTo-SecureString -String "$ENV:CodeSignCertPfxPassword" -AsPlainText -Force }
+        $params = @{
+            "codeSignCertPfxFile" = $codeSignCertPfxFile
+            "codeSignCertPfxPassword" = $codeSignCertPfxPassword
+        }
+    }
+    else {
+        $codeSignCertPfxPassword = $null
+    }
+}
+
+$allTestResults = "testresults*.xml"
+$testResultsFile = Join-Path $baseFolder "TestResults.xml"
+$testResultsFiles = Join-Path $baseFolder $allTestResults
+if (Test-Path $testResultsFiles) {
+    Remove-Item $testResultsFiles -Force
+}
+
+Run-AlPipeline @params `
+    -pipelinename $pipelineName `
+    -containerName $containerName `
+    -imageName $imageName `
+    -artifact $artifact.replace('{INSIDERSASTOKEN}',$insiderSasToken) `
+    -memoryLimit $memoryLimit `
+    -baseFolder $baseFolder `
+    -licenseFile $LicenseFile `
+    -installApps $installApps `
+    -previousApps $previousApps `
+    -appFolders $appFolders `
+    -testFolders $testFolders `
+    -doNotRunTests:$doNotRunTests `
+    -testResultsFile $testResultsFile `
+    -testResultsFormat 'JUnit' `
+    -installTestFramework:$installTestFramework `
+    -installTestLibraries:$installTestLibraries `
+    -installPerformanceToolkit:$installPerformanceToolkit `
+    -enableCodeCop:$enableCodeCop `
+    -enableAppSourceCop:$enableAppSourceCop `
+    -enablePerTenantExtensionCop:$enablePerTenantExtensionCop `
+    -enableUICop:$enableUICop `
+    -azureDevOps:($environment -eq 'AzureDevOps') `
+    -gitLab:($environment -eq 'GitLab') `
+    -AppSourceCopMandatoryAffixes $appSourceCopMandatoryAffixes `
+    -AppSourceCopSupportedCountries $appSourceCopSupportedCountries `
+    -additionalCountries $additionalCountries `
+    -buildArtifactFolder $buildArtifactFolder `
+    -CreateRuntimePackages:$CreateRuntimePackages `
+    -appBuild $appBuild -appRevision $appRevision
+
+if ($environment -eq 'AzureDevOps') {
+    Write-Host "##vso[task.setvariable variable=TestResults]$allTestResults"
+}
+
