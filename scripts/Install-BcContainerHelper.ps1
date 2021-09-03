8360ab7eb501ca2186344c146fa9672b05adb1a4
Freddy Kristiansen <freddyk@microsoft.com>
2021-09-03 08:26:58 +0200

    extended off

--- scripts/Install-BcContainerHelper.ps1
+++ scripts/Install-BcContainerHelper.ps1
@@ -57,3 +57,4 @@ if ($genericImageName) {
 }
 
 $bcContainerHelperConfig.TelemetryConnectionString = "InstrumentationKey=84bd9223-67d4-4378-8590-9e4a46023be2;IngestionEndpoint=https://westeurope-1.in.applicationinsights.azure.com/"
+$bcContainerHelperConfig.UseExtendedTelemetry = $false
